408a8af2aafdcee7615f288d6faa735b27dde4a5
EdrinM22 <emolla22@epoka.edu.al>
2024-05-16 19:41:38 +0200

    fix contact view

--- hotel_backend/contact_us/views.py
+++ hotel_backend/contact_us/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework.response import Response
 from rest_framework import status
-
-from .serializers import ContactListSerializer, ContactCreateSerializer
+from .serializers import ContactCreateSerializer, ContactListSerializer
 from .models import Contact
-
-
-# Create your views here.
 
 
 class ContactCreateAPIView(CreateAPIView):
@@ -16,9 +12,9 @@ class ContactCreateAPIView(CreateAPIView):
 
     def create(self, request, *args, **kwargs):
         response = super(ContactCreateAPIView, self).create(request, *args, **kwargs)
-        if response.status in [status.HTTP_200_OK, status.HTTP_201_CREATED]:
+        if response.status_code in [status.HTTP_200_OK, status.HTTP_201_CREATED]:
             pass
-            # do something...
+            # Do something if needed...
         return response
 
 
