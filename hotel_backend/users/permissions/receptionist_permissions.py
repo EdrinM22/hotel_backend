a5ab722d1ff03d49500506e22b9755a170eeeef5
erlik <ekuka21@epoka.edu.al>
2024-05-19 02:01:43 +0200

    Reservation and RoomTypes

--- hotel_backend/users/permissions/receptionist_permissions.py
+++ hotel_backend/users/permissions/receptionist_permissions.py
@@ -1,5 +1,7 @@
 from rest_framework.permissions import BasePermission
 
+from feedback.models import Feedback
+from hotel_reservation.models import Reservation
 from users.models import Receptionist, Admin, HotelManager
 
 
@@ -7,3 +9,6 @@ class ReceptionistPermission(BasePermission):
     def has_permission(self, request, view):
         return Receptionist.objects.filter(user=request.user).exists() or Admin.objects.filter(
             user=request.user).exists() or HotelManager.objects.filter(user=request.user).exists()
+
+    def has_object_permission(self, request, view, obj):
+        return True
